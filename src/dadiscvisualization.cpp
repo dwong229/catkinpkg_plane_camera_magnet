f9236a23b7b492820f77ae0cf415789ef89681ed
dwong229 <denwong@seas.upenn.edu>
2015-12-09 09:55:18 -0500

    Tracks one disc.  Need to add multiple disc tracking.

--- src/dadiscvisualization.cpp
+++ src/dadiscvisualization.cpp
@@ -1,22 +1,21 @@
 //disc visualization:
 
 #include <ros/ros.h>
-#include <plane_camera_magnet/xyFiltered.h>
-#include "filter.h"
-#include <Eigen/Geometry>
+#include <plane_camera_magnet/xyPix.h>
 #include <visualization_msgs/Marker.h> //for rvis visualization
 #include <math.h>
 #include <opencv2/opencv.hpp>
 
 using namespace std;
 
-static ros::Publisher kfmarker_pub;
-static ros::Publisher rawmarker_pub;
-static ros::Publisher kfxy_pub;
-static KalmanFilter kf;
+static ros::Publisher discmarker_pub_;
 
-static plane_camera_magnet::xyFiltered magnetactual_msg;
+static plane_camera_magnet::xyPix msg;
 static visualization_msgs::Marker rawpoints;
+
+double pix2m;
+double centerpixx;
+double centerpixy;
 
 class Visualizer
 {
@@ -25,20 +24,33 @@ public:
     
     void disccb(const plane_camera_magnet::xyPix& msg) {
 
+        
+        ROS_INFO_STREAM("msg: " << msg.magx[0] << " , " << msg.magy[0]);
+        
+        //convert to mm
+        double xyWorldX;
+        double xyWorldY;
+        xyWorldX = (msg.magx[0] - centerpixx)/pix2m;
+        xyWorldY = (msg.magy[0] + centerpixy)/pix2m;
+    
+
         discmarker.header.seq = msg.header.seq;
         discmarker.header.stamp = msg.header.stamp;
         discmarker.header.frame_id = "camera_frame";
-        discmarker.type = visualization_msgs::Marker::POINTS;
+        discmarker.type = visualization_msgs::Marker::CYLINDER;
+        discmarker.pose.position.x = xyWorldX;
+        discmarker.pose.position.y = xyWorldY;
+        discmarker.pose.position.z = 0.0;
         discmarker.pose.orientation.x = 0.0;
         discmarker.pose.orientation.y = 0.0;
-        discmarker.pose.orientation.z = 1.0 * sin(magnet_orientation/2);
-        discmarker.pose.orientation.w = cos(magnet_orientation/2);
+        discmarker.pose.orientation.z = 0.0;
+        discmarker.pose.orientation.w = 1.0;
         discmarker.scale.x = 1;
-        discmarker.scale.y = .4;
-        discmarker.scale.z = .5;
+        discmarker.scale.y = 1;
+        discmarker.scale.z = .3;
         discmarker.color.a = 1.0; // Don't forget to set the alpha!
-        discmarker.color.r = 0.0;
-        discmarker.color.g = 1.0;
+        discmarker.color.r = 1.0;
+        discmarker.color.g = 0.0;
         discmarker.color.b = 0.0;
 
 
@@ -49,7 +61,7 @@ public:
 
 
     Visualizer()  {
-        discpose_sub_ = n_.subscribe("/", 10, &Visualizer::disccb, this);
+        discpose_sub_ = n_.subscribe("/datrackdisc/xyPix", 10, &Visualizer::disccb, this);
         discmarker_pub_ = n_.advertise<visualization_msgs::Marker>("visualization_disc", 1, this);
     }
 
@@ -58,15 +70,23 @@ private:
   ros::NodeHandle n_{"~"};
   ros::Subscriber discpose_sub_;
   ros::Publisher discmarker_pub_;
-
   visualization_msgs::Marker discmarker;
 };
 
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
+
   ros::init(argc, argv, "dadiscvisualization"); // initialize node: 
   ros::NodeHandle n("~");
+
+  std::string cal_file;    
+  n.param("cal_file", cal_file, std::string("calplane.yml"));
+  ROS_INFO_STREAM("cal file " << cal_file);
+  cv::FileStorage fscal(cal_file.c_str(), cv::FileStorage::READ);
+  centerpixx = (double)fscal["coilavgx"];
+  centerpixy = (double)fscal["coilavgy"];
+  pix2m = (double)fscal["pix2m"];
 
   Visualizer viz;
   
